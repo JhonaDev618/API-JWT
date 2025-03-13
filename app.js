34bd2dcb620f54262d9d281854ad61725303d7d8
Jhonatan <jhonatam.mattoss@hotmail.com>
2025-03-12 21:23:19 -0300

    feat:Adicionado e configurado Swagger

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import publicRoutes from "./routes/public.js";
 import "dotenv/config"; // Importa as variáveis de ambiente no .env variable
+import swaggerDocs from "./swaggerConfig.js";
 
 const app = express();
 const port = process.env.PORT; // Define a porta do servidor
@@ -8,6 +9,9 @@ app.use(express.json()); // define que o express vai utilizar json
 
 // Rotas públicas do arquivo routes.
 app.use("/", publicRoutes);
+
+// Configurar o Swagger
+swaggerDocs(app);
 
 /*
 Agora vamos evoluir a API criada na atividade anterior:
--- routes/public.js
+++ routes/public.js
@@ -9,6 +9,42 @@ const router = express.Router();
 
 const SECRET = process.env.SECRET;
 
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Realiza o login de um usuário e retorna o token JWT.
+ *     description: Autentica o usuário e gera um token JWT para futuras requisições.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               login:
+ *                 type: string
+ *               senha:
+ *                 type: integer
+ *             required:
+ *               - login
+ *               - senha
+ *     responses:
+ *       200:
+ *         description: Login bem-sucedido, retorna o token JWT.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 auth:
+ *                   type: boolean
+ *                   example: true
+ *                 token:
+ *                   type: string
+ *       401:
+ *         description: Login ou senha inválidos.
+ */
 router.post("/login", (req, res) => {
   const login = req.body.login;
   const senha = parseInt(req.body.senha);
@@ -28,13 +64,65 @@ router.post("/login", (req, res) => {
       .status(401)
       .json({ Menssage: "Erro ao realizar login no sistema" });
   }
-   
 });
 
-router.get("/users", verifyJwt, (req, res) => {
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retorna todos os usuários.
+ *     description: Recupera a lista de usuários cadastrados.
+ *     responses:
+ *       200:
+ *         description: Lista de usuários.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                   nome:
+ *                     type: string
+ *                   email:
+ *                     type: string
+ */
+router.get("/users", (req, res) => {
   res.status(200).json(usuariosControler.listarUsuarios());
 });
 
+/**
+ * @swagger
+ * /users/{id}:
+ *   get:
+ *     summary: Retorna um usuário pelo ID.
+ *     description: Busca um usuário com base no ID informado.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: ID do usuário
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Usuário encontrado.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 nome:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *       400:
+ *         description: ID inválido.
+ */
 router.get("/users/:id", verifyJwt, (req, res) => {
   const id = parseInt(req.params.id);
   const retorno = usuariosControler.buscarUsuarioID(id);
@@ -44,6 +132,32 @@ router.get("/users/:id", verifyJwt, (req, res) => {
   res.status(200).json(retorno);
 });
 
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Cria um novo usuário.
+ *     description: Cadastra um novo usuário no sistema.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *             required:
+ *               - nome
+ *               - email
+ *     responses:
+ *       201:
+ *         description: Usuário criado com sucesso.
+ *       400:
+ *         description: Nome e email são obrigatórios.
+ */
 router.post("/users", verifyJwt, (req, res) => {
   const novoUsuario = req.body; //recebendo os dados do usuario enviados no POST para a api
   if (!novoUsuario.nome || !novoUsuario.email) {
@@ -53,6 +167,52 @@ router.post("/users", verifyJwt, (req, res) => {
   res.status(201).json(novoUsuario);
 });
 
+/**
+ * Definições adicionais da documentação Swagger usando anotações.
+ *
+ * @swagger
+ * /users/{id}:
+ *   put:
+ *     summary: Atualiza os dados de um usuário pelo ID.
+ *     description: Atualiza as informações de um usuário específico, identificando-o pelo ID.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: ID do usuário a ser atualizado
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nome:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *             required:
+ *               - nome
+ *               - email
+ *     responses:
+ *       200:
+ *         description: Usuário atualizado com sucesso.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                 nome:
+ *                   type: string
+ *                 email:
+ *                   type: string
+ *       400:
+ *         description: ID inválido ou dados inválidos.
+ */
 router.put("/users/:id", verifyJwt, (req, res) => {
   const id = parseInt(req.params.id);
   const usuarioAtualizado = req.body;
@@ -63,6 +223,23 @@ router.put("/users/:id", verifyJwt, (req, res) => {
   res.status(200).json(a);
 });
 
+/* /users/{id}:
+ *   delete:
+ *     summary: Remove um usuário pelo ID.
+ *     description: Exclui um usuário específico, identificado pelo ID.
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         description: ID do usuário a ser excluído
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       204:
+ *         description: Usuário excluído com sucesso, sem conteúdo na resposta.
+ *       400:
+ *         description: ID inválido.
+ */
 router.delete("/users/:id", verifyJwt, (req, res) => {
   const id = parseInt(req.params.id);
   usuariosControler.removerUsuario(id);
